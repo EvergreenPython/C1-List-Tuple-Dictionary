d5bcd6f717169a116ac213f98cad83271fd750f5
PythonEESD2021 <ctran@eesd.org>
2021-02-26 00:31:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+02/25/2021
+
+List
+A data structure that is mutable (changable) ordered sequence of elements. Defined by values between square brackets, [ ].
+
+Ex.
+students = ["student1", "student2", "student3", ....]
+
+box = ["Anything", 123, True, 4.28, -100]
+print (box)
+>> ["Anything", 123, True, 4.28, -100]
+
+name = ["John", "Chloe", "Ian", "Ashley"]
+print (name)
+>> ["John", "Chloe", "Ian", "Ashley"]
+'''
